70687b3c7e6afb728dc1d330bf4453dda8057a37
Garrett Honeycutt <code@garretthoneycutt.com>
2020-02-20 00:38:14 -0500

    attempt to fix appveyor

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -7,6 +7,11 @@ run_puppet_install_helper
 install_module_dependencies
 install_module
 
+# TODO does this solve https://ci.appveyor.com/project/ghoneycutt/puppet-module-facter/build/job/mj98gonri15otyqh
+if Gem.win_platform?
+  set :backend, :cmd
+end
+
 RSpec.configure do |c|
   # Readable test descriptions
   c.formatter = :documentation
